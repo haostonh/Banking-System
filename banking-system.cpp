6de270abf507ab28f04902c884c06e618ba9eff3
haostonh <hsthuynh@ucdavis.edu>
2022-02-14 16:35:53 -0800

    money transaction functions

--- banking-system.cpp
+++ banking-system.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
 
 class Customer {
-
+  public:
+    void DepositMoney(float money) {
+      balance += money;
+      on_hand_balance -= money;
+    }
+    void GetBalance() {std::cout << balance << std::endl;}
+    void WithdrawMoney(float money) {
+      balance -= money;
+      on_hand_balance += money;
+    }
+  private:
+    float bank_balance, on_hand_balance;
 };
 
 int main() {
