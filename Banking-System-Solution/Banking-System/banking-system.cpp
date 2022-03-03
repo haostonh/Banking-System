c99b484ae981dabdf9ac515cab2471b5ec5b7c77
haostonh <hsthuynh@ucdavis.edu>
2022-03-03 14:07:47 -0800

    Include mysql and cppconn headers

--- Banking-System-Solution/Banking-System/banking-system.cpp
+++ Banking-System-Solution/Banking-System/banking-system.cpp
@@ -5,6 +5,11 @@
 #include<string>
 #include<vector>
 
+#include"mysql_connection.h"
+#include<cppconn/driver.h>
+#include<cppconn/exception.h>
+#include<cppconn/prepared_statement.h>
+
 class Customer {
 public:
     void DepositMoney(float money) {
