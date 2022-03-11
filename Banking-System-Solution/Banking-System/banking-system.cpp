95f48a2b47ad29664ffe8da854edfa18f32796fe
haostonh <hsthuynh@ucdavis.edu>
2022-03-11 14:28:50 -0800

    Remove extra headers

--- Banking-System-Solution/Banking-System/banking-system.cpp
+++ Banking-System-Solution/Banking-System/banking-system.cpp
@@ -5,13 +5,10 @@
 #include<string>
 #include<vector>
 
-#include"mysql_driver.h"
 #include"mysql_connection.h"
 #include<cppconn/driver.h>
 #include<cppconn/exception.h>
 #include<cppconn/statement.h>
-#include<cppconn/prepared_statement.h>
-#include<cppconn/datatype.h>
 
 class Customer {
 public:
